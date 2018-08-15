2273a3c4124554a4cf6e48788220bb9049a6584c
FletcherFT <fletcher.thompson@utas.edu.au>
2018-08-15 22:10:43 +1000

    added node to publish unfiltered odometry (for comparison against EKF/UKF)

--- asv_navigation/scripts/imu_transform_broadcaster.py
+++ asv_navigation/scripts/imu_transform_broadcaster.py
@@ -2,16 +2,12 @@
 import rospy
 import tf_conversions
 import tf2_ros
-#from tf import transformations as T
 from sensor_msgs.msg import Imu
 from math import sqrt, pi
 from geometry_msgs.msg import Vector3, TransformStamped
 
 def imu_cb(msg):
     br = tf2_ros.TransformBroadcaster()
-    quat = (msg.orientation.x,msg.orientation.y,msg.orientation.z,msg.orientation.w)
-    eul = tf_conversions.transformations.euler_from_quaternion(quat)
-    rospy.logdebug("| Roll {}\t| Pitch {}\t| Yaw {} |".format(eul[0]*180.0/pi,eul[1]*180.0/pi,eul[2]*180.0/pi))
     T = TransformStamped()
     T.header.stamp = rospy.Time.now()
     T.header.frame_id = "odom"
@@ -19,11 +15,11 @@ def imu_cb(msg):
     T.transform.translation.x = 0
     T.transform.translation.y = 0
     T.transform.translation.z = 0
-    T.transform.rotation.x = quat[0]
-    T.transform.rotation.y = quat[1]
-    T.transform.rotation.z = quat[2]
-    T.transform.rotation.w = quat[3]
+    T.transform.rotation = msg.orientation
     br.sendTransform(T)
+    quat = (msg.orientation.x,msg.orientation.y,msg.orientation.z,msg.orientation.w)
+    eul = tf_conversions.transformations.euler_from_quaternion(quat,axes='sxyz')
+    rospy.logdebug("| Roll {}\t| Pitch {}\t| Yaw {} |".format(eul[0]*180.0/pi,eul[1]*180.0/pi,eul[2]*180.0/pi))
 
 if __name__ == '__main__':
     rospy.init_node('imu_viz')
