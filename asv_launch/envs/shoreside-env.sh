cbf1b0e3b8fc5aae5d28568753f98a77f32c82aa
FletcherFT <fletcher.thompson@utas.edu.au>
2018-08-16 14:45:01 +1000

    Updating remote launch

--- asv_launch/envs/shoreside-env.sh
+++ asv_launch/envs/shoreside-env.sh
@@ -3,4 +3,5 @@ source /opt/ros/kinetic/setup.bash
 source $HOME/ROS_ws/devel/setup.bash
 export ROS_MASTER_URI=http://asv-server.local:11311
 export ROS_HOSTNAME=$HOSTNAME.local
+export DISPLAY=:0
 exec "$@"
