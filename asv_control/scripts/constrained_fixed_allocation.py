f3e7deb98d5b1189837267d63b81409694a3a863
FletcherFT <fletcher.thompson@utas.edu.au>
2018-09-05 18:29:59 +1000

    staging for control mode manager and topic muxing for hardware control.

--- asv_control/scripts/constrained_fixed_allocation.py
+++ asv_control/scripts/constrained_fixed_allocation.py
@@ -7,6 +7,7 @@ import numpy as np
 from scipy.linalg import block_diag
 from math import cos, sin, pi, radians, degrees
 from asv_control_msgs.msg import Thrusters
+from asv_control_msgs.srv import ConfigureSteppers
 import quadprog
 
 def quadprog_solve_qp(P, q, G=None, h=None, A=None, b=None):
@@ -26,7 +27,23 @@ class ConstrainedNonrotatableAllocation:
     def __init__(self):
         rospy.init_node("control_allocation")
         # default is AP mode
-        self.thruster = rospy.get_param('thrusterDP')
+        self.qpsetup(True)
+        self.thrust_msg = Thrusters()
+        self.thrust_pub = rospy.Publisher("thrusters",Thrusters,queue_size=10)
+        self.sol_msg = WrenchStamped()
+        self.sol_pub = rospy.Publisher("tau_sol",WrenchStamped,queue_size=10)
+        self.mode_server = rospy.Service('modeconfig',ConfigureSteppers,self.mode)
+        rospy.Subscriber("tau_com",WrenchStamped,self.wrenchCallback)
+        rospy.spin()
+
+    def qpsetup(self,mode):
+        if mode:
+            self.thruster = rospy.get_param('thrusterAP')
+        else:
+            self.thruster = rospy.get_param('thrusterDP')
+        self.kpwm = self.thruster['Kpwm']
+        self.deadband = self.thruster['deadband']
+        self.krpm = self.thruster['Krpm']
         self.r = 3
         self.n = self.thruster['n']
         self.W = diag(self.thruster['W'])
@@ -55,12 +72,6 @@ class ConstrainedNonrotatableAllocation:
             [zeros((self.r,self.n+self.r)),eye(self.r)],
             [zeros((2*self.r,self.n+self.r*2))]
         ])
-        self.thrust_msg = Thrusters()
-        self.thrust_pub = rospy.Publisher("thruster",Thrusters,queue_size=10)
-        self.sol_msg = WrenchStamped()
-        self.sol_pub = rospy.Publisher("tau_sol",WrenchStamped,queue_size=10)
-        rospy.Subscriber("tau_com",WrenchStamped,self.wrenchCallback)
-        rospy.spin()
 
     def wrenchCallback(self,msg):
         tau_com = array([msg.wrench.force.x,msg.wrench.force.y,msg.wrench.torque.z])
@@ -98,11 +109,41 @@ class ConstrainedNonrotatableAllocation:
         self.thrust_msg.pwm = self.forceToPWM(thrusts)
         self.thrust_pub.publish(self.thrust_msg)
 
+    def mode(self,request):
+        try:
+            self.qpsetup(request.mode)
+            if request.mode:
+                S="Configuring for AP mode"
+            else:
+                S="Configuring for DP mode"
+        except Exception as exc:
+            response=[False,str(exc)]
+        else:
+            rospy.logdebug(S)
+            response=[True,S]
+        return response
+
     def forceToRPM(self,thrusts):
-        return [6,6,6]
+        rpm = [0,0,0]
+        for idx,i in enumerate(thrusts):
+            if i>0:
+                rpm[idx]=self.krpm[idx]*i**2
+            else:
+                rpm[idx]=-self.krpm[idx]*i**2
+        return rpm
     
     def forceToPWM(self,thrusts):
-        return [100,100,100]
+        pwm = [0,0,0]
+        for idx,i in enumerate(thrusts):
+            if i>0:
+                pwm[idx]=self.kpwm[idx]*i**2#+self.deadband[idx]
+                if abs(pwm[idx])<self.deadband[idx]:
+                    pwm[idx]=0
+            else:
+                pwm[idx]=-self.kpwm[idx]*i**2#-self.deadband[idx]
+                if abs(pwm[idx])<self.deadband[idx]:
+                    pwm[idx]=0
+        return pwm
 
 if __name__ == "__main__":
     try:
