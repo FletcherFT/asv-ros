2882f3c96cbf458c9c5c158eaf93f8d9d2528899
FletcherFT <fletcher.thompson@utas.edu.au>
2018-09-13 16:06:29 +1000

    Tested SIL for MTB mission

--- asv_mission/scripts/testmissionlistener.py
+++ asv_mission/scripts/testmissionlistener.py
@@ -4,17 +4,19 @@ from asv_mission.msg import Plan, Task
 from missionplanning import hierarchy
 
 def receivePlanCallback(plan_msg):
+    rospy.loginfo("Plan received: {}".format(plan_msg))
     plan = hierarchy.Hierarchy(plan_msg)
     while True:
         task = plan.getTask()
         print(task)
         if task.action == "root":
             break
+        plan.taskDone()
     print "Plan complete."
 
 def main():
     rospy.init_node("testmissionlistener")
-    rospy.Subscriber("plan",Plan,receivePlanCallback)
+    rospy.Subscriber("mission/plan",Plan,receivePlanCallback)
     rospy.spin()
 
 if __name__=="__main__":
