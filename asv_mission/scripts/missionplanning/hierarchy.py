45ec72ab0674726793808cf7e2e627044eb35eef
FletcherFT <fletcher.thompson@utas.edu.au>
2018-09-21 03:28:16 +1000

    Given supervisor a marker array message so that operator can see the mission as it progresses.

--- asv_mission/scripts/missionplanning/hierarchy.py
+++ asv_mission/scripts/missionplanning/hierarchy.py
@@ -3,6 +3,7 @@ import task
 class Hierarchy():
     def __init__(self,plan_msg):
         root = None
+        self.home = None
         plan = plan_msg.plan
         for i in range(len(plan)):
             if plan[i].parent == -1:
@@ -11,6 +12,13 @@ class Hierarchy():
                 else:
                     print("ERROR: There is more than one root task.")
                     return
+            elif plan[i].action=="HOME":
+                if self.home is None:
+                    self.home = i
+                else:
+                    print("ERROR: There is more than one home task.")
+                    return
+                
         if root is None:
             print("ERROR: There is no root task.")
             return
@@ -21,48 +29,13 @@ class Hierarchy():
         self._complete = []
         self._root = root
         self._current = root
-        #self._flag = True
 
     def getTask(self):
-#        pdb.set_trace()
         self._findLowest()
-        # if self._flag:
-        #     # this is the first time the getTask function has been called for this plan, don't append anything to self._complete
-        #     self._findLowest()
-        #     self._flag=False
-        # else:
-        #     self._complete.append(self._current)
-        #     self._findLowest()
         return self.plan[self._current]
 
     def taskDone(self):
         self._complete.append(self._current)
-
-    # def _findLowest(self):
-    #     while True:
-    #         children = self.plan[self._current].children
-    #         if children:
-    #             # go through every child and find one that hasn't been completed
-    #             flag=False
-    #             for child in children:
-    #                 if not child in self._complete:
-    #                     # if there's a child that isn't completed, go down a level
-    #                     self._current=child
-    #                     flag=True
-    #                     break
-    #             if flag:
-    #                 # if a child was found that hasn't been completed yet, select it and see if it has any children
-    #                 continue
-    #             else:
-    #                 # otherwise, all the children are complete, therefore this current task is ready to be selected
-    #                 return
-    #         else:
-    #             # this task has no children, so check if it has been completed
-    #             if not self._current in self._complete:
-    #                 return
-    #             else:
-    #                 # the task has been completed, so go up a level
-    #                 self._current=self.plan[self._current].parent
 
     def _findLowest(self):
         while True:
--- asv_mission/scripts/supervisor.py
+++ asv_mission/scripts/supervisor.py
@@ -5,10 +5,13 @@ from sensor_msgs.msg import Joy
 from std_srvs.srv import Trigger, SetBool
 from std_msgs.msg import String
 from geographic_msgs.msg import GeoPoseStamped
-from geometry_msgs.msg import Quaternion, PoseStamped
+from geometry_msgs.msg import Quaternion, PoseStamped, Pose, Vector3
 from topic_tools.srv import MuxSelect
 from missionplanning import hierarchy
 from tf_conversions import transformations as tf
+from std_msgs.msg import ColorRGBA
+from visualization_msgs.msg import Marker, MarkerArray
+from pygeodesy import utm
 
 class Supervisor:
     def __init__(self):
@@ -19,6 +22,7 @@ class Supervisor:
         rospy.init_node("supervisor")
         self.task_pub = rospy.Publisher('guidance/task',GeoPoseStamped,queue_size=10)
         self.status_pub = rospy.Publisher("asv/status",String,queue_size=10)
+        self.marker_pub = rospy.Publisher("mission/markers",MarkerArray,queue_size=10)
         self.pause = rospy.Service('supervisor/pause', SetBool, self.pausePlayCallback)
         self.start = rospy.Service('supervisor/start',Trigger,self.startPlanCallback)
         self.nexttask = rospy.Service('supervisor/request_new', Trigger, self.nextTaskCallback)
@@ -61,7 +65,7 @@ class Supervisor:
         geo_msg = GeoPoseStamped()
         geo_msg.header.frame_id="utm"
         geo_msg.header.stamp = rospy.Time.now()
-        if task.action == "wp":
+        if task.action == "WP":
             # waypoint task, configure for AP mode to a GPS waypoint.
             if self.changeMode("/tau_com/AP"):
                 geo_msg.pose.position.altitude=-1
@@ -72,7 +76,7 @@ class Supervisor:
                 return [True,"Waypoint Task Selected, executing."]
             else:
                 return [False,"Mode not configured to AP."]
-        elif task.action == "hp":
+        elif task.action == "HP":
             # hold position task, configure DP mode at a GPS waypoint and orientation for a set time.
             if self.changeMode("/tau_com/DP"):
                 geo_msg.pose.position.altitude=0
@@ -85,7 +89,7 @@ class Supervisor:
                 return [True,"Hold Pose Task Selected, executing."]
             else:
                 return [False,"Mode not configured to DP."]
-        elif task.action == "root":
+        elif task.action == "ROOT":
             # root task reached, notify the operator that the mission is complete.
             if self.changeMode("__none"):
                 self.mode="idle"
@@ -93,8 +97,9 @@ class Supervisor:
                 rospy.loginfo("Final task completed, now idling.")
                 return [True,"Final task completed, now idling."]
             else:
+                return [False,"Couldn't Idle for some reason?"]
                 rospy.logerr("Couldn't disable output topic, but mission complete.")
-        elif task.action == "start":
+        elif task.action == "START":
             # start task, is a waypoint task to move the vehicle to the starting position.
             if self.changeMode("/tau_com/AP"):
                 geo_msg.pose.position.altitude=-1
@@ -103,8 +108,23 @@ class Supervisor:
                 geo_msg.pose.orientation = Quaternion(0,0,0,1)
                 self.task_pub.publish(geo_msg)
                 rospy.loginfo("Moving to starting waypoint of plan.")
+                return [True,"AP mode to starting point."]
             else:
                 rospy.logerr("Couldn't configure to AP mode.")
+                return [False,"Couldn't configure to AP mode."]
+        elif task.action == "HOME":
+            # start task, is a waypoint task to move the vehicle to the starting position.
+            if self.changeMode("/tau_com/AP"):
+                geo_msg.pose.position.altitude=-1
+                geo_msg.pose.position.latitude=task.data[0]
+                geo_msg.pose.position.longitude=task.data[1]
+                geo_msg.pose.orientation = Quaternion(0,0,0,1)
+                self.task_pub.publish(geo_msg)
+                rospy.loginfo("Moving to rendezvous point.")
+                return [True,"AP mode to home point."]
+            else:
+                rospy.logerr("Couldn't configure to AP mode.")
+                return [False,"Couldn't configure to AP mode."]
         else:
             rospy.logerr("Task of type {} is not supported, getting next task.".format(task.action))
             self.plan.taskDone()
@@ -115,7 +135,7 @@ class Supervisor:
             rospy.wait_for_service("control_topic_mux/select",5.0)
         except:
             rospy.logerr("No control_topic_mux/select service found, is controlmanager.py running?")
-            return [False,"No control_topic_mux/select service found, is controlmanager.py running?"]
+            return False
         service_handle = rospy.ServiceProxy('control_topic_mux/select', MuxSelect)
         service_handle(request)
         return True
@@ -123,7 +143,31 @@ class Supervisor:
     def receivePlanCallback(self,plan_msg):
         # parse the received Plan message
         self.plan = hierarchy.Hierarchy(plan_msg)
+        self.home_task = self.plan.home
+        self.updateMissionMarkers()
         rospy.loginfo("Plan received, waiting for start mission service on {}".format(rospy.resolve_name("supervisor/start")))
+
+    def updateMissionMarkers(self):
+        array_msg = MarkerArray()
+        array_msg.markers = []
+        for idx,task in enumerate(self.plan.plan):
+            marker_msg = Marker()
+            marker_msg.header.stamp = rospy.Time.now()
+            marker_msg.header.frame_id = "utm"
+            if task.action=="WP" or task.action=="HOME":
+                marker_msg.ns = "mission/markers"
+                marker_msg.id = idx
+                marker_msg.type = 2
+                marker_msg.action = 0
+                UTM = utm.toUtm(task.data[0],task.data[1])
+                marker_msg.pose = Pose(Vector3(UTM.easting,UTM.northing,0),Quaternion(0,0,0,1))
+                marker_msg.scale = Vector3(5,5,5)
+                if idx in self.plan._complete:
+                    marker_msg.color = ColorRGBA(0,1,0,0.8) #GREEN
+                else:
+                    marker_msg.color = ColorRGBA(1,0,0,0.8) #RED
+                array_msg.markers.append(marker_msg)
+        self.marker_pub.publish(array_msg)
 
     def nextTaskCallback(self,request):
         # new task requested from the guidance node.
@@ -132,6 +176,7 @@ class Supervisor:
             rospy.sleep(rospy.Duration(self.hptime))
             self.hptask = False
         self.plan.taskDone()
+        self.updateMissionMarkers()
         rospy.loginfo("Task done, getting next...")
         return self.parseTask(self.plan.getTask())
 
