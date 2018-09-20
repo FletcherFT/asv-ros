f7f8ef83a0b534ba69eed3279e29247b5a642183
FletcherFT <fletcher.thompson@utas.edu.au>
2018-09-20 18:34:57 +1000

    Added service to reset energy counter..

--- asv_energy/scripts/energy_monitor.py
+++ asv_energy/scripts/energy_monitor.py
@@ -2,24 +2,30 @@
 import rospy
 from sensor_msgs.msg import BatteryState
 from asv_energy.msg import Float64Stamped
+from std_srvs.srv import Trigger
 
 class EnergyMonitor:
     def __init__(self):
         rospy.init_node('energy_monitor')
         self._flag = False
+        self._prev_energy = 0
         self._power_pub = rospy.Publisher('energy/power',Float64Stamped,queue_size=10)
         self._energy_pub = rospy.Publisher('energy/aggregate',Float64Stamped,queue_size=10)
         self._power_msg = Float64Stamped()
         self._energy_msg = Float64Stamped()
+        self._energy_datum_srv = rospy.Service("energy/datum",Trigger,self.datumCallback)
         rospy.Subscriber("energy/battery",BatteryState,self.batteryCallback)
         rospy.spin()
+
+    def datumCallback(self,request):
+        self._prev_energy = 0
+        return [True,"Reset energy aggregator."]
 
     def batteryCallback(self,msg):
         if not self._flag:
             self._flag = True
             self._stamp = msg.header.stamp
             power = msg.voltage*msg.current
-            self._prev_energy = 0
             self._prev_power = power
         else:
             dt = (msg.header.stamp - self._stamp).to_sec()
