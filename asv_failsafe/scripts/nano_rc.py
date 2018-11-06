4f902678905fadb0a94edb946e37f99ef82fe3ab
Fletcher Thompson <Fletcher.Thompson@utas.edu.au>
2018-11-06 16:44:45 +1100

    rosifying

--- asv_failsafe/scripts/nano_rc.py
+++ asv_failsafe/scripts/nano_rc.py
@@ -1,19 +1,48 @@
+#!/usr/bin/python
+import rospy
 import serial
 import struct
+import numpy as np
+from sensor_msgs.msg import Joy
+from std_srvs.srv import SetBool, Trigger
 
-def main():
-    try:
-        fmt = ">BBBBBB"
-        size = struct.calcsize(fmt)
-        with serial.Serial("/dev/ttyUSB0",9600,timeout=1) as ser:
-            while True:
-                line = ser.readline().rstrip()
-                if not line is None and len(line)==size:
-                    data = struct.unpack(fmt,line)
-                    print(data)
-    except KeyboardInterrupt:
-        pass
+class nano_rc():
+    def __init__(self):
+        rospy.init_node("rc_read")
+        self._PORT = rospy.get_param("~device","/dev/ttyUSB0")
+        self._BAUD = rospy.get_param("~baud",9600)
+        self._TIMEOUT = rospy.get_param("~timeout",1.0)
+        self.joy_msg = Joy()
+        self.pub = rospy.Publisher("joy",Joy,queue_size=10)
+        self.main()
 
+    def main(self):
+        try:
+            fmt = ">BBBBBB"
+            size = struct.calcsize(fmt)
+            with serial.Serial(self._PORT,self._BAUD,timeout=self._TIMEOUT) as ser:
+                while not rospy.is_shutdown():
+                    line = ser.readline().rstrip()
+                    if not line is None and len(line)==size:
+                        data = struct.unpack(fmt,line)
+                        rospy.logdebug(data)
+                        self.parse_joystick(data)
+        except rospy.ROSInterruptException:
+            pass
+
+    def parse_joystick(self,data):
+        data = np.array(data)
+        data = data/255.0
+        ax = data[0:4]
+        but = data[4:]
+        but[but<0.5]=0
+        but[but>0.5]=1
+        self.joy_msg.axes = ax
+        self.joy_msg.buttons = but
+        rospy.logdebug(data)
+        self.joy_msg.header.stamp = rospy.Time.now()
+        self.joy_msg.header.frame_id = "override"
+        self.pub.publish(self.joy_msg)
 
 if __name__=="__main__":
-    main()
+    nano_rc()
