7efc3c776a88c7d1423e4d53b62e1156ef2a681c
FletcherFT <fletcher.thompson@utas.edu.au>
2018-11-06 16:22:56 +1100

    6 Byte Message Protocol

--- asv_failsafe/scripts/nano_rc.py
+++ asv_failsafe/scripts/nano_rc.py
@@ -3,13 +3,17 @@ import struct
 
 def main():
     try:
-        fmt = "cHHHHHH"
-        with serial.Serial("/dev/ttyACM0",9600,timeout=1) as ser:
+        fmt = ">BBBBBB"
+        size = struct.calcsize(fmt)
+        with serial.Serial("/dev/ttyUSB0",9600,timeout=1) as ser:
             while True:
-                line = ser.readline()
-                if line not None:
+                line = ser.readline().rstrip()
+                if not line is None and len(line)==size:
                     data = struct.unpack(fmt,line)
                     print(data)
+    except KeyboardInterrupt:
+        pass
+
 
 if __name__=="__main__":
     main()
